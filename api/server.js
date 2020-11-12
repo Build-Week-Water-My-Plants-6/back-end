24da4e756793342a296701f68f66ea4c3f5f0c9e
Kyle Leonard <kdleonard@icloud.com>
2020-11-11 19:46:34 -0600

    Basic server setup.
    Will need to add more code for the routers

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+server.get("/", (req, res) => {
+  res.send("Water My Plant");
+});
+
+module.exports = server;
