65a22e8477ebe3a389c39d433ada63074e4d1903
Kyle Leonard <kdleonard@icloud.com>
2020-11-12 19:46:37 -0600

    Finished plants seed file

--- database/seeds/02-plants.js
+++ database/seeds/02-plants.js
@@ -1,13 +1,56 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("plants")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("plants").insert([
+        {
+          plant_name: "Abutilon",
+          plant_species: "Flowering Maple",
+          water_schedule: "Water in 1 day"
+        },
+        {
+          plant_name: "Summer Bloom",
+          plant_species: "Ageratum",
+          water_schedule: "Water in 2 day"
+        },
+        {
+          plant_name: "Aloe Vera",
+          plant_species: "Aloe barbadensis",
+          water_schedule: "Water in 3 day"
+        },
+        {
+          plant_name: "Barberton Daisy",
+          plant_species: "Gebera Jamesonii",
+          water_schedule: "Water in 4 day"
+        },
+        {
+          plant_name: "Bird of Paradise",
+          plant_species: "Strelitzia Reginae",
+          water_schedule: "Water in 5 day"
+        },
+        {
+          plant_name: "Busy Lizzie",
+          plant_species: "Impatiens Walleriana",
+          water_schedule: "Water in 6 day"
+        },
+        {
+          plant_name: "Calla lily",
+          plant_species: "Zantedeschia aethiopica",
+          water_schedule: "Water in 7 day"
+        },
+        {
+          plant_name: "Eternal Flame",
+          plant_species: "Calathea Crocata",
+          water_schedule: "Water in 8 day"
+        },
+        {
+          plant_name: "Lollipop",
+          plant_species: "Pachystachys Lutea",
+          water_schedule: "Water in 9 day"
+        }
       ]);
     });
 };
+
