07a8c2bfd1f68896aaf18a5eaaefa0d08c6df4f7
Kyle Leonard <kdleonard@icloud.com>
2020-11-12 20:04:23 -0600

    Finished migrations file

--- database/migrations/20201111185959_create-user.js
+++ database/migrations/20201111185959_create-user.js
@@ -1,8 +1,27 @@
-
 exports.up = function(knex) {
+    return (
+      knex.schema
+        .createTable("users", tbl => {
+          tbl.increments();
+          tbl.string("fullname", 128).notNullable();
+          tbl
+            .string("username", 128)
+            .notNullable()
+            .unique();
+          tbl.string("password", 128).notNullable();
+          tbl.string("phonenumber", 11);
+        })
+        .createTable("plants", tbl => {
+          tbl.increments();
+          tbl.string("plant_name", 256).notNullable();
+          tbl.string("plant_species", 256).notNullable();
+          tbl.string("water_schedule", 512).notNullable();
+        })
+    );
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    // reverse order of creation
+    return knex.schema.dropTableIfExists("plants").dropTableIfExists("users");
+  };
   
-};
