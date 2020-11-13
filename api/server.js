b9e2840ae742f9353813e713ea9bcbd828773b86
Kyle Leonard <kdleonard@icloud.com>
2020-11-12 20:07:34 -0600

    Finished up the server

--- api/server.js
+++ api/server.js
@@ -5,9 +5,17 @@ require("dotenv").config();
 
 const server = express();
 
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const plantsRouter = require("../plants/plants-router.js");
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/plants", plantsRouter);
 
 
 server.get("/", (req, res) => {
