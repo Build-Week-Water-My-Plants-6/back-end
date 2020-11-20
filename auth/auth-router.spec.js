9268c7ba4c8d31d5a8f7d9d34c1c7f5bea307e9a
Kyle Leonard <kdleonard@icloud.com>
2020-11-19 19:12:48 -0600

    Finished up testing files

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -12,23 +12,36 @@ describe("GET /", () => {
   });
 });
 
-describe("POST /api/plants", () => {
+describe("POST /register", () => {
   it("should return JSON", () => {
     return request(server)
-      .post("/api/plants")
+      .post("/api/auth/register")
       .send({
-        plant_name: "test5",
-        plant_species: "test5",
-        water_schedule: "test5"
+        fullname: "testroute",
+        username: "testroute11",
+        password: "abc123",
+        phonenumber: "12125555559"
       })
       .then(res => {
         expect(res.type).toMatch(/json/);
       });
   });
+
+  it("should return 200", () => {
+    return request(server)
+      .post("/api/auth/login")
+      .send({
+        username: "testroute11",
+        password: "abc123"
+      })
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
 });
 
-describe("GET /api/plants", () => {
-  it("should return list of plants", () => {
+describe("GET /api/users", () => {
+  it("should return list of users", () => {
     return request(server)
       .get("/")
       .then(res => {
